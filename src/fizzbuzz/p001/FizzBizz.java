d3f17521308911cd1f3ac20bf78a542d09ed81da
leeonky <leeonky@gmail.com>
2016-04-01 15:40:03 +0000

    refactor

--- src/fizzbuzz/p001/FizzBizz.java
+++ src/fizzbuzz/p001/FizzBizz.java
@@ -1,14 +1,15 @@
 package fizzbuzz.p001;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class FizzBizz {
 
 	public static List<String> count(int start, int end) {
+		List<String> results = new ArrayList<>();
 		if (start == 1 && end == 2)
 			return Arrays.asList("1", "2");
-		return Arrays.asList(String.valueOf(start));
+		results.add(String.valueOf(start));
+		return results;
 	}
 
 }
