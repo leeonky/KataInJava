4d88b7129d7f5ffdc9a2135ca8c7cdd90bda1e69
leeonky <leeonky@gmail.com>
2016-04-01 09:42:28 +0000

    Two factors 2 passed

--- src/primeFactor/p001/Factor.java
+++ src/primeFactor/p001/Factor.java
@@ -7,7 +7,9 @@ public class Factor {
 	public static List<Integer> factorsOf(int number) {
 		if (number == 1)
 			return Arrays.asList();
-		return Arrays.asList(2);
+		if (number == 2)
+			return Arrays.asList(2);
+		return Arrays.asList(2, 2);
 	}
 
 }
