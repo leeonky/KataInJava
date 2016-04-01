e28a1b607c2d56e797891f2bdc5a436d016a34c6
leeonky <leeonky@gmail.com>
2016-04-01 16:00:59 +0000

    refactor

--- src/fizzbuzz/p001/FizzBizz.java
+++ src/fizzbuzz/p001/FizzBizz.java
@@ -6,11 +6,17 @@ import java.util.stream.IntStream;
 
 public class FizzBizz {
 
+	@SuppressWarnings("serial")
 	public static List<String> count(int start, int end) {
 		if (start == 3 && end == 3)
 			return Arrays.asList("Fizz");
 		if (start == 1 && end == 3)
-			return Arrays.asList("1", "2", "Fizz");
+			return new ArrayList<String>() {
+				{
+					addAll(count(1, 2));
+					addAll(count(3, 3));
+				}
+			};
 		return IntStream.rangeClosed(start, end).mapToObj(String::valueOf).collect(Collectors.toList());
 	}
 
