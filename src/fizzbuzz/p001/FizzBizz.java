a2c7559ef348ba00d208fbb501f88117566f6ae6
leeonky <leeonky@gmail.com>
2016-04-01 15:45:55 +0000

    pass the case

--- src/fizzbuzz/p001/FizzBizz.java
+++ src/fizzbuzz/p001/FizzBizz.java
@@ -7,6 +7,8 @@ import java.util.stream.IntStream;
 public class FizzBizz {
 
 	public static List<String> count(int start, int end) {
+		if (start == 3)
+			return Arrays.asList("Fizz");
 		return IntStream.rangeClosed(start, end).mapToObj(String::valueOf).collect(Collectors.toList());
 	}
 
