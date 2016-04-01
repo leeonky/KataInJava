b7e1042d43b40d876490855fa4b1b7f829223251
leeonky <leeonky@gmail.com>
2016-04-01 15:43:17 +0000

    refactor to Stream

--- src/fizzbuzz/p001/FizzBizz.java
+++ src/fizzbuzz/p001/FizzBizz.java
@@ -1,14 +1,13 @@
 package fizzbuzz.p001;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class FizzBizz {
 
 	public static List<String> count(int start, int end) {
-		List<String> results = new ArrayList<>();
-		for (int i = start; i <= end; ++i)
-			results.add(String.valueOf(i));
-		return results;
+		return IntStream.rangeClosed(start, end).mapToObj(String::valueOf).collect(Collectors.toList());
 	}
 
 }
