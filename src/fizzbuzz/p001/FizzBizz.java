24a3bcd2e3e10c0d42a4b913dea40d036a89cbed
leeonky <leeonky@gmail.com>
2016-04-01 15:41:19 +0000

    refactor

--- src/fizzbuzz/p001/FizzBizz.java
+++ src/fizzbuzz/p001/FizzBizz.java
@@ -6,9 +6,8 @@ public class FizzBizz {
 
 	public static List<String> count(int start, int end) {
 		List<String> results = new ArrayList<>();
-		if (start == 1 && end == 2)
-			return Arrays.asList("1", "2");
-		results.add(String.valueOf(start));
+		for (int i = start; i <= end; ++i)
+			results.add(String.valueOf(i));
 		return results;
 	}
 
