c9d3d13a9b2987a244c8aff1c659ffc1f52c6e5d
leeonky <leeonky@gmail.com>
2016-04-02 02:54:03 +0000

    pass the case

--- src/fizzbuzzwhizz/FizzBuzzWhizz.java
+++ src/fizzbuzzwhizz/FizzBuzzWhizz.java
@@ -1,12 +1,13 @@
 package fizzbuzzwhizz;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class FizzBuzzWhizz {
 
 	public static List<String> count(int number) {
-		return Arrays.asList("1");
+		return IntStream.rangeClosed(1, number).mapToObj(String::valueOf).collect(Collectors.toList());
 	}
 
 }
