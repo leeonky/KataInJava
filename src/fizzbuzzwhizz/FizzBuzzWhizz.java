20a147f4cbf346992fa94b491564ad161b6bd3ef
leeonky <leeonky@gmail.com>
2016-04-02 03:02:55 +0000

    refactor

--- src/fizzbuzzwhizz/FizzBuzzWhizz.java
+++ src/fizzbuzzwhizz/FizzBuzzWhizz.java
@@ -7,10 +7,10 @@ import java.util.stream.IntStream;
 public class FizzBuzzWhizz {
 
 	public static List<String> count(int number) {
-		if (number == 3)
-			return Arrays.asList("1", "2", "Fizz");
 		List<String> list = IntStream.rangeClosed(1, number).mapToObj(String::valueOf)
 				.collect(Collectors.toCollection(ArrayList<String>::new));
+		if (number == 3)
+			list.set(number - 1, "Fizz");
 		return list;
 	}
 
