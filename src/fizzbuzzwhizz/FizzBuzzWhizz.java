de03d24ef0c0b04f9eca4176dbccabbe6e2c0d83
leeonky <leeonky@gmail.com>
2016-04-02 03:15:39 +0000

    refactor

--- src/fizzbuzzwhizz/FizzBuzzWhizz.java
+++ src/fizzbuzzwhizz/FizzBuzzWhizz.java
@@ -7,12 +7,20 @@ import java.util.stream.IntStream;
 public class FizzBuzzWhizz {
 
 	public static List<String> count(int number) {
-		List<String> list = IntStream.rangeClosed(1, number).mapToObj(String::valueOf)
-				.collect(Collectors.toCollection(ArrayList<String>::new));
-		IntStream.rangeClosed(1, number / 3).map(i -> i * 3).forEach(i -> list.set(i - 1, "Fizz"));
+		List<String> list = convertNumberSequenceToStringFormat(number);
+		sequenceTo(number).forEach(i -> list.set(i - 1, "Fizz"));
 		if (number >= 5)
 			list.set(4, "Buzz");
 		return list;
+	}
+
+	private static IntStream sequenceTo(int number) {
+		return IntStream.rangeClosed(1, number / 3).map(i -> i * 3);
+	}
+
+	private static ArrayList<String> convertNumberSequenceToStringFormat(int number) {
+		return IntStream.rangeClosed(1, number).mapToObj(String::valueOf)
+				.collect(Collectors.toCollection(ArrayList<String>::new));
 	}
 
 }
