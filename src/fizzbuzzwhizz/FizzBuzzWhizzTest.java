8877fdd5d5a6eadf1e5994aea1183db1566807f9
leeonky <leeonky@gmail.com>
2016-04-02 02:52:00 +0000

    1 to 2

--- src/fizzbuzzwhizz/FizzBuzzWhizzTest.java
+++ src/fizzbuzzwhizz/FizzBuzzWhizzTest.java
@@ -11,6 +11,7 @@ public class FizzBuzzWhizzTest {
 	@Test
 	public void no_fizz_buzz_whizz() {
 		assertEquals(Arrays.asList("1"), FizzBuzzWhizz.count(1));
+		assertEquals(Arrays.asList("1", "2"), FizzBuzzWhizz.count(2));
 	}
 
 }
