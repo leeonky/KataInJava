26bb6631bebe17ea3404ea8690e8c03a03491b8a
leeonky <leeonky@gmail.com>
2016-04-02 02:48:19 +0000

    only number

---
+++ src/fizzbuzzwhizz/FizzBuzzWhizz.java
@@ -0,0 +1,11 @@
+package fizzbuzzwhizz;
+
+import java.util.List;
+
+public class FizzBuzzWhizz {
+
+	public static List<String> count(int number) {
+		return null;
+	}
+
+}
---
+++ src/fizzbuzzwhizz/FizzBuzzWhizzTest.java
@@ -0,0 +1,16 @@
+package fizzbuzzwhizz;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class FizzBuzzWhizzTest {
+
+	@Test
+	public void no_fizz_buzz_whizz() {
+		assertEquals(Arrays.asList("1"), FizzBuzzWhizz.count(1));
+	}
+
+}
