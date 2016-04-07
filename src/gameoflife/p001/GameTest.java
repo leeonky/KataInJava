7cb99812f458ad3adc2b4230700db2f665f1d8dd
leeonky <leeonky@gmail.com>
2016-04-07 10:39:17 +0000

    topright bottomleft

--- src/gameoflife/p001/GameTest.java
+++ src/gameoflife/p001/GameTest.java
@@ -53,4 +53,16 @@ public class GameTest {
 
 		assertEquals(new HashSet<>(Arrays.asList(new Point(2, 2))), game.alives());
 	}
+
+	@Test
+	public void one_alive_cell_with_two_top_right_left_bottom_alive_cell_shall_alive_in_next_generation() {
+		Game game = new Game(3, 3);
+		game.addAlive(new Point(3, 1));
+		game.addAlive(new Point(2, 2));
+		game.addAlive(new Point(1, 3));
+
+		game.generate();
+
+		assertEquals(new HashSet<>(Arrays.asList(new Point(2, 2))), game.alives());
+	}
 }
