c168243a5c6f1f5bbe0fb3058d00e5b1fa41cb85
leeonky <leeonky@gmail.com>
2016-04-07 08:26:52 +0000

    pass number '1' and '2'

--- src/fizzbuzzwhizz/p002/FizzBuzzWhizz.java
+++ src/fizzbuzzwhizz/p002/FizzBuzzWhizz.java
@@ -1,11 +1,13 @@
 package fizzbuzzwhizz.p002;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class FizzBuzzWhizz {
 
 	public static List<String> count(int to) {
-		return Arrays.asList(String.valueOf(to));
+		return IntStream.rangeClosed(1, to).mapToObj(String::valueOf).collect(Collectors.toList());
 	}
 
 }
