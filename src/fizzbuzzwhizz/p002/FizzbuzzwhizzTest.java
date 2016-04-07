f62627379290de83cfcd62a92bb1f4d21d994c10
leeonky <leeonky@gmail.com>
2016-04-07 08:30:36 +0000

    refactor test names

--- src/fizzbuzzwhizz/p002/FizzbuzzwhizzTest.java
+++ src/fizzbuzzwhizz/p002/FizzbuzzwhizzTest.java
@@ -11,6 +11,11 @@ public class FizzbuzzwhizzTest {
 	@Test
 	public void only_number() {
 		assertEquals(Arrays.asList("1"), FizzBuzzWhizz.count(1));
+		assertEquals(Arrays.asList("1", "2"), FizzBuzzWhizz.count(2));
 	}
 
+	@Test
+	public void one_fizz() {
+		assertEquals(Arrays.asList("1", "2", "Fizz"), FizzBuzzWhizz.count(3));
+	}
 }
