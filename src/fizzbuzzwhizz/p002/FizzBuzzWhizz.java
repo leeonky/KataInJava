652290b4a8a81ba410e8885b9d078da5d418c6b9
leeonky <leeonky@gmail.com>
2016-04-07 08:40:38 +0000

    refactor

--- src/fizzbuzzwhizz/p002/FizzBuzzWhizz.java
+++ src/fizzbuzzwhizz/p002/FizzBuzzWhizz.java
@@ -7,11 +7,15 @@ import java.util.stream.IntStream;
 public class FizzBuzzWhizz {
 
 	public static List<String> count(int to) {
-		List<String> result = IntStream.rangeClosed(1, to).mapToObj(String::valueOf).collect(Collectors.toList());
+		List<String> result = mapSequenceInStringList(to);
 		IntStream.rangeClosed(1, to / 3).map(i -> i * 3 - 1).forEach(i -> result.set(i, "Fizz"));
 		if (5 <= to)
 			result.set(4, "Buzz");
 		return result;
+	}
+
+	private static List<String> mapSequenceInStringList(int to) {
+		return IntStream.rangeClosed(1, to).mapToObj(String::valueOf).collect(Collectors.toList());
 	}
 
 }
