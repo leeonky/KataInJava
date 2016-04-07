8d1df747ffe5f141059e7a03a6655df260848b10
leeonky <leeonky@gmail.com>
2016-04-07 11:12:45 +0000

    dead to alive

--- src/gameoflife/p001/Game.java
+++ src/gameoflife/p001/Game.java
@@ -2,12 +2,17 @@ package gameoflife.p001;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Game {
 
 	Set<Point> alives = new HashSet<>();
+	private final int width;
+	private final int height;
 
 	public Game(int width, int height) {
+		this.width = width;
+		this.height = height;
 	}
 
 	public void addAlive(Point alive) {
@@ -15,11 +20,21 @@ public class Game {
 	}
 
 	public void generate() {
+		Set<Point> newAlive = canAliveFromDeadInNextGeneration().collect(Collectors.toSet());
 		alives = alives.stream().filter(this::canSurviveInNextGeneration).collect(Collectors.toSet());
+		alives.addAll(newAlive);
+		alives = alives.stream().filter(p -> p.x <= width && p.y <= height && p.x > 0 && p.y > 0)
+				.collect(Collectors.toSet());
+	}
+
+	private Stream<Point> canAliveFromDeadInNextGeneration() {
+		return alives.stream().flatMap(Point::getNeighours).filter(p -> !isAlive(p))
+				.filter(p -> p.getNeighours().filter(this::isAlive).count() == 3);
 	}
 
 	private boolean canSurviveInNextGeneration(Point target) {
-		return target.getNeighours().filter(this::isAlive).count() == 2;
+		return target.getNeighours().filter(this::isAlive).count() == 2
+				|| target.getNeighours().filter(this::isAlive).count() == 3;
 	}
 
 	private boolean isAlive(Point target) {
--- src/gameoflife/p001/GameTest.java
+++ src/gameoflife/p001/GameTest.java
@@ -65,4 +65,30 @@ public class GameTest {
 
 		assertEquals(new HashSet<>(Arrays.asList(new Point(2, 2))), game.alives());
 	}
+
+	@Test
+	public void one_alive_cell_with_three_alive_cells_shall_alive_in_next_generation() {
+		Game game = new Game(2, 2);
+		game.addAlive(new Point(1, 1));
+		game.addAlive(new Point(1, 2));
+		game.addAlive(new Point(2, 1));
+		game.addAlive(new Point(2, 2));
+
+		game.generate();
+
+		assertEquals(new HashSet<>(Arrays.asList(new Point(1, 1), new Point(1, 2), new Point(2, 1), new Point(2, 2))),
+				game.alives());
+	}
+
+	@Test
+	public void one_dead_cell_with_three_alive_cells_shall_alive_in_next_generation() {
+		Game game = new Game(2, 3);
+		game.addAlive(new Point(1, 1));
+		game.addAlive(new Point(2, 2));
+		game.addAlive(new Point(1, 3));
+
+		game.generate();
+
+		assertEquals(new HashSet<>(Arrays.asList(new Point(1, 2), new Point(2, 2))), game.alives());
+	}
 }
