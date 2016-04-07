9f5678e60c4b6a949163ccee8f0e6101c4dc47d8
leeonky <leeonky@gmail.com>
2016-04-07 08:48:20 +0000

    refactor

--- src/fizzbuzzwhizz/p002/FizzBuzzWhizz.java
+++ src/fizzbuzzwhizz/p002/FizzBuzzWhizz.java
@@ -8,18 +8,15 @@ public class FizzBuzzWhizz {
 
 	public static List<String> count(int to) {
 		List<String> result = mapSequenceInStringList(to);
-		processFizzRule(to, result);
-		if (5 <= to)
-			result.set(4, "Buzz");
-		if (10 <= to)
-			result.set(9, "Buzz");
+		processFizzRule(3, to, result, "Fizz");
+		processFizzRule(5, to, result, "Buzz");
 		if (7 <= to)
 			result.set(6, "Whizz");
 		return result;
 	}
 
-	private static void processFizzRule(int to, List<String> result) {
-		IntStream.rangeClosed(1, to / 3).map(i -> i * 3 - 1).forEach(i -> result.set(i, "Fizz"));
+	private static void processFizzRule(int step, int to, List<String> result, String word) {
+		IntStream.rangeClosed(1, to / step).map(i -> i * step - 1).forEach(i -> result.set(i, word));
 	}
 
 	private static List<String> mapSequenceInStringList(int to) {
